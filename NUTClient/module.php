3e8062f87f5fdbe302d1490454aecf9dd324b3b9
IP-Symcon <root@ips-prod>
2019-12-31 12:41:43 +0100

    initial

--- NUTClient/module.php
+++ NUTClient/module.php
@@ -1,35 +1,36 @@
 <?php
-	class NUTClient extends IPSModule {
 
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
+declare(strict_types=1);
+class NUTClient extends IPSModule
+{
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
 
-			$this->ConnectParent("{6179ED6A-FC31-413C-BB8E-1204150CF376}");
-		}
+        $this->ConnectParent('{6179ED6A-FC31-413C-BB8E-1204150CF376}');
+    }
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+    public function Destroy()
+    {
+        //Never delete this line!
+        parent::Destroy();
+    }
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+    }
 
-		public function Send(string $Text)
-		{
-			$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $Text)));
-		}
+    public function Send(string $Text)
+    {
+        $this->SendDataToParent(json_encode(['DataID' => '{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}', 'Buffer' => $Text]));
+    }
 
-		public function ReceiveData($JSONString)
-		{
-			$data = json_decode($JSONString);
-			IPS_LogMessage("Device RECV", utf8_decode($data->Buffer));
-		}
-
-	}
+    public function ReceiveData($JSONString)
+    {
+        $data = json_decode($JSONString);
+        IPS_LogMessage('Device RECV', utf8_decode($data->Buffer));
+    }
+}
